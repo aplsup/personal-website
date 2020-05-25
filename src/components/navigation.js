78e18ec38a86142d00c79446e6dec44a97424829
Alessio Paciello <alessio.paciello@treatwell.com>
2020-05-25 09:39:28 +0100

    Removed contact link

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,9 +3,8 @@ import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
-  <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
+  <nav className="navigation">
     <ThemeChanger/>
   </nav>
-  
+
 )
